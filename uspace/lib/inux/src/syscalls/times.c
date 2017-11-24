38309b793be66825443f2316bb649d0fb2b75259
Vojtech Horky <vojtech.horky@gmail.com>
2017-11-24 13:30:56 +0100

    libinux: sys_times naive implementation

---
+++ uspace/lib/inux/src/syscalls/times.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017 Vojtech Horky
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @addtogroup libinux
+ * @{
+ */
+
+#include "syscalls.h"
+#include "consts.h"
+#include "libinux.h"
+#include <stdlib.h>
+
+typedef long clock_t;
+
+struct tms {
+	clock_t tms_utime;
+	clock_t tms_stime;
+	clock_t tms_cutime;
+	clock_t tms_cstime;
+};
+
+static clock_t counter = 0;
+
+DEFINE_LINUX_SYSCALL1(times, struct tms *, tms)
+{
+	logger(LVL_DEBUG, "times(tms=%p)", tms);
+
+	// FIXME: count the time properly :-)
+	counter += 100;
+
+	tms->tms_utime = counter / 2;
+	tms->tms_stime = counter / 2;
+	tms->tms_cutime = counter / 4;
+	tms->tms_cstime = counter / 4;
+
+	return counter;
+}
+
+
+/** @}
+ */
